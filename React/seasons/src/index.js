34469a9ebd948b8c86e611cc907e0a193e7f4cb4
David Park <xdavidpark@gmail.com>
2019-06-25 23:54:29 -0700

    Add seasonDisplay, refactor, and add css

--- React/seasons/src/index.js
+++ React/seasons/src/index.js
@@ -1,25 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';
 
 class App extends React.Component {
-  constructor(props){
-    super(props);
+  // constructor(props){
+  //   super(props);
 
-    this.state = {
-      lat: null,
-      errorMessage: ''
-    };
+  //   this.state = {
+  //     lat: null,
+  //     errorMessage: ''
+  //   };
+  // }
 
+  state = { lat: null, errorMessage: '' };
+
+  componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(
-      (position) => {
-        this.setState({ lat: position.coords.latitude });
-        console.log(position)
-      },
-      (err) => {
-        this.setState({ errorMessage: err.message })
-      }
+      (position) => this.setState({ lat: position.coords.latitude }),
+      (err) => this.setState({ errorMessage: err.message })
     );
   }
+
   
   render(){
     if (this.state.errorMessage && !this.state.lat) {
@@ -27,10 +28,10 @@ class App extends React.Component {
     }
 
     if (!this.state.errorMessage && this.state.lat) {
-      return <div>Latitude: {this.state.lat}</div>;
+      return <SeasonDisplay lat={this.state.lat}/>;
     }
 
-    return <div>Loading!</div>;
+    return <div>Loading!</div>; 
   }
 };
 
