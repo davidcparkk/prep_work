42d3ed48e9d1389862f4582c10d7497962379aa1
David Park <xdavidpark@gmail.com>
2019-06-30 17:34:39 -0700

    Add search method

--- React/pics/src/components/SearchBar.js
+++ React/pics/src/components/SearchBar.js
@@ -14,13 +14,15 @@ class SearchBar extends React.Component {
   onFormSubmit(event){
     event.preventDefault();
 
-    console.log(this.state.term);
+    // console.log(this.state.term);
+
+    this.props.onSubmit(this.state.term);
   }
 
   render() {
     return(
        <div className="ui segment">
-         <form onSubmit={() => this.onFormSubmit()} className="ui form">
+         <form onSubmit={(e) => this.onFormSubmit(e)} className="ui form">
             <div className="field">
               <label>Image Search</label>
               <input type = "text" value={this.state.term} onChange={(e) => this.setState({ term: e.target.value})} />
