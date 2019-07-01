1a8f7fa2ad6280222f5fa4e792765565da51f829
David Park <xdavidpark@gmail.com>
2019-06-30 22:15:15 -0700

    Add image list items from api request

--- React/pics/src/components/App.js
+++ React/pics/src/components/App.js
@@ -1,16 +1,14 @@
 import React from 'react';
-import axios from 'axios';
+import unsplash from '../api/unsplash';
 import SearchBar from './SearchBar';
+import ImageList from './ImageList';
 
 class App extends React.Component {
   state = { images: [] };
 
   onSearchSubmit = async (term) => {
-    const response = await axios.get('https://api.unsplash.com/search/photos',{
+    const response = await unsplash.get('/search/photos',{
       params: { query: term },
-      headers: {
-        Authorization: 'Client-ID f13c50006d7d63bf5c56d39572a4a1cd3f9d6dfed7fc453ad6504155a7671b68'
-      }
     });
     
     this.setState({ images: response.data.results });
@@ -25,7 +23,7 @@ class App extends React.Component {
     return (
       <div className="ui container" style={{ marginTop: '10px'}}>
         <SearchBar onSubmit={this.onSearchSubmit} />
-          Found: {this.state.images.length} images
+        <ImageList images={this.state.images} />
       </div>
     )
   }
