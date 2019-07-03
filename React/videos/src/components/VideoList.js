730bca7176a58a45151f5d8d302a5dfbef6aa03f
David Park <xdavidpark@gmail.com>
2019-07-02 22:59:44 -0700

    Add video list using response data

---
+++ React/videos/src/components/VideoList.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import VideoItem from './VideoItem';
+
+const VideoList = ({ videos }) => {
+  // props.videos
+  const renderedList = videos.map( video => {
+    return <VideoItem />;
+  });
+
+  return(
+    <div>{renderedList}</div>
+  )
+}
+
+export default VideoList;
