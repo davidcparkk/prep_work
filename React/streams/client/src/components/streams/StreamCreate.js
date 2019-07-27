f122c802229fd8fd7b75336ed422b8d1b4ec2fb0
David Park <xdavidpark@gmail.com>
2019-07-26 23:56:24 -0700

    Add redux forms

--- React/streams/client/src/components/streams/StreamCreate.js
+++ React/streams/client/src/components/streams/StreamCreate.js
@@ -1,9 +1,47 @@
 import React from 'react';
+import { Field, reduxForm } from 'redux-form';
 
-const StreamCreate = () => {
-  return(
-    <div>StreamCreate</div>
-  )
+class StreamCreate extends React.Component {
+  renderInput({ input, label, meta }) {
+    return (
+      <div className="field">
+        <label>{label}</label>
+        <input {...input} />
+        <div>{meta.error}</div>
+      </div>
+    )
+  }
+
+  onSubmit(formValues){
+    
+  }
+
+  render() {
+    return(
+      <form className="ui form" onSubmit={this.props.handleSubmit(this.onSubmit)}>
+        <Field label="Enter Title" name="title" component={this.renderInput} />
+        <Field label="Enter Description" name="description" component={this.renderInput} />
+        <button className="ui button primary">Submit</button>
+      </form>
+    )
+  }
 }
 
-export default StreamCreate;
+const validate = (formValues) => {
+  const errors = {};
+
+  if(!formValues.title) {
+    errors.title = 'You must enter a title';
+  }
+
+  if (!formValues.description){
+    errors.description = 'You must enter a descrption';
+  }
+
+  return errors;
+}
+
+export default reduxForm({
+  form: 'streamCreate',
+  validate
+})(StreamCreate);
