88216e985fa8a6c06ba124fcc84bdab7b56bb98b
David Park <xdavidpark@gmail.com>
2019-09-16 23:19:19 -0700

    Add residences to maps

--- geosite/geosite/src/components/Map.js
+++ geosite/geosite/src/components/Map.js
@@ -1,9 +1,53 @@
 import React from 'react';
+import { Map, GoogleApiWrapper, Marker } from 'google-maps-react';
 
-class Map extends React.Component {
+class MapContainer extends React.Component {
+  constructor (props){
+    super(props);
+
+    this.state={
+      employers: [{
+        lat: 34.0522, lng: -118.2437
+      }],
+      residences:[{
+        lat: 37.8044, lng: -122.2712
+      }]
+
+    }
+  }
+
+  displayMarkers = () => {
+    return this.state.residences.map( (residence, index) => {
+      return <Marker key={index} id={index} position={{
+        lat: residence.lat,
+        lng: residence.lng
+      }}
+      onClick={() => console.log("You clicked me!")}
+      />
+    })
+  }
+  
+
   render() {
-    return <div>Map</div>;
+    const mapStyles = {
+      width: '50%',
+      height: '50%',
+    };
+
+    return <div>
+      <Map 
+        google={this.props.google}
+        zoom={8}
+        style={mapStyles}
+        initialCenter={{ lat: 37.7749, lng: -122.4194 }}
+      >
+        {this.displayMarkers()}
+      </Map>
+        
+    </div>;
   }
 }
 
-export default Map;
+export default GoogleApiWrapper({
+  apiKey: 'AIzaSyC3K5QN8HIQwZwa-fb2iGL868KC6z9J3ZE'
+})(MapContainer);
