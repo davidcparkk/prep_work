ba07b6c402629cabbce25aa6eca8ac8c6b3d8b97
David Park <xdavidpark@gmail.com>
2019-09-16 22:18:29 -0700

    Initialize coding challenge

---
+++ geosite/geosite/src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Map from './Map';
+
+class App extends React.Component {
+  render(){
+    return(
+      <div>
+        <Map />
+      </div>
+    )
+  }
+};
+
+export default App;
