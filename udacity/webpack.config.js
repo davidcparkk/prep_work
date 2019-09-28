484fb739afb78f40a577238be376087a5c3930f8
David Park <xdavidpark@gmail.com>
2019-09-27 22:25:46 -0700

    git cahce cleared

---
+++ udacity/webpack.config.js
@@ -0,0 +1,41 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const path = require("path");
+
+
+module.exports = {
+  entry: './src/app.js',
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [{ loader: 'style-loader'}, { loader: 'css-loader' }]
+      }
+    ]
+  },
+  devServer: {
+    contentBase: './dist',
+    open: true
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "webpack Output",
+    }),
+    new CleanWebpackPlugin()
+  ],
+  
+};
