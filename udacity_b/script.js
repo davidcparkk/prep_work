2c5c486e676c9fd14fb6bc74ef3e98d83bbb8958
David Park <xdavidpark@gmail.com>
2019-09-28 01:02:19 -0700

    Add css

--- udacity_b/script.js
+++ udacity_b/script.js
@@ -54,21 +54,14 @@ function submitForm(path) {
   }).then(function(datums){
     console.log(datums);
     removeLoading();
+    clearErrors();
     const message = document.querySelector('#message');
     message.classList.add('messageDisplay');
     message.innerText = `${datums.statusText.message}`;
-    // innerText not HTML
-    // name/for labelinput
-    // collects input
-      // input.addEventListener('click', function())
-    // webpack, keeps functions from being global
-    // document.createElement, findByID, appendChild
-
-    // message, I can show you my react. i do have a lot of exp with react and I this project would be a great fit for it, would be happy to do it again if you are intersrted in seeing.
-
   }).catch(function(err){
     console.log(err);
     removeLoading();
+    clearErrors();
     const message = document.querySelector('#message');
     message.classList.add('messageDisplay');
     message.innerText = `${err.statusText.message}`;;
@@ -76,48 +69,46 @@ function submitForm(path) {
 }
 
 function validateForm(action){
-  
+  clearErrors();
   const username = document.querySelector('#username').value;
   const password = document.querySelector('#password').value;
   const email = document.querySelector('#email').value;
 
   if(username === ""){
     const error = document.createElement('span');
+    error.classList.add('error');
     var node = document.createTextNode('Username must be filled out.');
     error.appendChild(node);
     const element = document.getElementById('username');
     element.parentNode.insertBefore(error, element.nextSibling);
-    return false;
-    // alert("Username must be filled out. Try again!")
-    // return false;
   }
 
   if(email === ""){
     const error = document.createElement('span');
+    error.classList.add('error');
     var node = document.createTextNode('Email must be filled out.');
     error.appendChild(node);
     const element = document.getElementById('email');
     element.parentNode.insertBefore(error, element.nextSibling);
-    return false;
-    // alert("Email must be filled out. Try again!")
-    // return false;
   }
 
   if(password === "" || password.length < 6){
     const error = document.createElement('span');
+    error.classList.add('error');
     var node = document.createTextNode('Password must be filled out and be at least 6 characters long.');
     error.appendChild(node);
     const element = document.getElementById('password');
     element.parentNode.insertBefore(error, element.nextSibling);
-    return false;
-    // alert("Password must be filled out and at least 6 characters long");
-    // return false;
   }
   
-  if(action.id === "sign-up"){
-    submitForm(API_PATH_SIGNUP);
-  } else if (action.id === "sign-in"){
-    submitForm(API_PATH_SIGNIN);
+  if(password !== "" && password.length >=6 && username !== "" && email !== ""){
+    if(action.id === "sign-up"){
+      submitForm(API_PATH_SIGNUP);
+    } else if (action.id === "sign-in"){
+      submitForm(API_PATH_SIGNIN);
+    }
+  } else {
+    return false;
   }
 }
 
@@ -132,17 +123,7 @@ function removeLoading(){
   document.getElementById("loading").style.display = "none";
 }
 
-// change class to input invalid, errors
-// one label id = label, id = label errors
+function clearErrors() {
+  document.querySelectorAll('.error').forEach(e => e.parentNode.removeChild(e));
 
-// semantic HTML, don't want any divs, sections, tables, form, button. Want HTMl as semantic. make page simpler to look at by just, prset styles
-
-// page grays out and loading spinner, submission sussccessfull
-// click here to view details, show JSON data
-// redirect to another html
-
-// DOM manipulation is synchronous, loadingBar visilibyt: true, make this APi request.then
-// or axios/fetch
-
-// get webpack set up
-// webpack to bundle toegether
+}
