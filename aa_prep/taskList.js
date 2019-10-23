2dc16b0609175997fe975ee0bfbf719ca84ad41e
David Park <xdavidpark@gmail.com>
2019-10-22 23:13:35 -0700

    Add task list class async queue

---
+++ aa_prep/taskList.js
@@ -0,0 +1,40 @@
+class TaskList{
+  constructor(maxConcurrency){
+    this.count = 0;
+    this.queue = [];
+    this.maxConcurrency = maxConcurrency;
+  }
+
+  run(arg){
+
+
+    if(this.count < this.maxConcurrency){
+      this.count++;
+      return new Promise(function(resolve){
+        
+        setTimeout(function(){
+          console.log('done')
+          resolve();
+        },1000);
+
+        console.log('running');
+        
+      }).then( (el) => {
+        this.count--;
+        if(this.queue.length){
+          let nextJob = this.queue.shift();
+          this.run(nextJob);
+        }
+        
+      });
+      
+    } else {
+      this.queue.push(arg)
+    }
+  }
+}
+
+let task = new TaskList(3);
+for(let i = 0; i < 9; i++){
+  task.run(i)
+}
