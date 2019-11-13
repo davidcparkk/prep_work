a85b4a130ae31d4ece4ee6baf934bbc80768ef05
David Park <xdavidpark@gmail.com>
2019-11-12 18:43:27 -0800

    Add helper formatDate

--- ember/my-app/app/controllers/comment.js
+++ ember/my-app/app/controllers/comment.js
@@ -1,7 +1,18 @@
 import Controller from '@ember/controller';
 
 export default Controller.extend({
+  actions: {
+    sayHello: () => alert('Say hello'),
+    toggleBody: function(){
+      this.toggleProperty('isShowingBody')
+    },
+    handleSubmit: function(){
+      alert(this.get('name'));
+      alert(this.get('content'));
+    }
+  },
   title: 'my comment',
+  created: new Date(),
   body: 'this is the body of my comments',
   authors: ['john', 'david', 'norma'],
   comments: [
