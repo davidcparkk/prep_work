22a726307f44f3939c1019d15c1a21a5ca876cc9
David Park <xdavidpark@gmail.com>
2019-11-12 16:48:18 -0800

    Add bootstrap styling and routing for services

---
+++ ember/my-app/app/routes/services.js
@@ -0,0 +1,8 @@
+import Ember from 'ember';
+import Route from '@ember/routing/route';
+
+export default Route.extend({
+  model: function(){
+    return ['Repairs', 'Diagnostics', 'Search'];
+  }
+});
